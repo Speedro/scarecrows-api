37500de94030423f18919ca2f9f42f7cbc262bd7
Petr Kadlec <the.swdev@gmail.com>
2022-12-24 15:01:06 +0100

    - events api updates

--- api/src/main/java/cz/scarecrows/eventmanager/mapper/EntityMapper.java
+++ api/src/main/java/cz/scarecrows/eventmanager/mapper/EntityMapper.java
@@ -5,8 +5,10 @@
 package cz.scarecrows.eventmanager.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import cz.scarecrows.eventmanager.data.TeamEventDto;
+import cz.scarecrows.eventmanager.data.request.TeamEventRequest;
 import cz.scarecrows.eventmanager.model.TeamEvent;
 
 @Mapper(componentModel = "spring")
@@ -15,4 +17,7 @@ public interface EntityMapper {
     TeamEventDto toDto(final TeamEvent teamEvent);
 
     TeamEvent toEntity(final TeamEventDto teamEventDto);
+
+    @Mapping(target = "id", ignore = true)
+    TeamEvent toEntity(final TeamEventRequest teamEventRequest);
 }
