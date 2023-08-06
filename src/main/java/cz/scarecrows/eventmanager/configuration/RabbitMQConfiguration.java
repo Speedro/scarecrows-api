c1cffd3430639839815b241a0bfa3256f5bc0722
Petr Kadlec <petr.kadlec@finshape.com>
2023-08-06 13:18:20 +0200

    rabbitmq fixup

--- src/main/java/cz/scarecrows/eventmanager/configuration/RabbitMQConfiguration.java
+++ src/main/java/cz/scarecrows/eventmanager/configuration/RabbitMQConfiguration.java
@@ -1,9 +1,6 @@
 package cz.scarecrows.eventmanager.configuration;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,29 +12,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfiguration {
 
-    @Value("${rabbitmq.queue.name}")
-    private String queueName;
-
-    @Value("${rabbitmq.exchange.name}")
-    private String exchangeName;
-
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
+    @Value("${spring.rabbitmq.uri}")
+    private String uri;
 
     @Bean
-    public Queue queue() {
-        return new Queue(queueName);
-    }
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(exchangeName);
-    }
-
-    // binding between queue and exchange using routing key
-    @Bean
-    public Binding binding() {
-        return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+    public ConnectionFactory connectionFactory() {
+        final CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setUri(uri);
+        return connectionFactory;
     }
 
     // needed to send messages in application/json format
