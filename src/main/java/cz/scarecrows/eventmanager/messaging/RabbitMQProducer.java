91cef35ac99248dbbed0649d41d16a1e36e1b0e3
Petr Kadlec <93187845+Speedro@users.noreply.github.com>
2023-08-06 13:20:09 +0200

    rabbitmq fixup (#17)
    Co-authored-by: Petr Kadlec <petr.kadlec@finshape.com>

--- src/main/java/cz/scarecrows/eventmanager/messaging/RabbitMQProducer.java
+++ src/main/java/cz/scarecrows/eventmanager/messaging/RabbitMQProducer.java
@@ -1,7 +1,6 @@
 package cz.scarecrows.eventmanager.messaging;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import cz.scarecrows.eventmanager.data.request.MemberRegistrationRequest;
@@ -13,17 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RabbitMQProducer {
 
-    @Value("${rabbitmq.exchange.name}")
-    private String exchange;
-
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
-
     private final RabbitTemplate rabbitTemplate;
 
     public void sendMessage(final MemberRegistrationRequest registrationRequest) {
         log.debug("Sending registration request {}", registrationRequest);
-        rabbitTemplate.convertAndSend(exchange, routingKey, registrationRequest);
+        rabbitTemplate.convertAndSend("registration-request-exchange", "registration-status-update", registrationRequest);
     }
 
 }
