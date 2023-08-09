8a901a5760e6c46d52f10af5c8106cdd19ba9d08
Petr Kadlec <petr.kadlec@finshape.com>
2023-08-09 19:31:23 +0200

    changes - cross origin fix attempt 7

--- src/main/java/cz/scarecrows/eventmanager/controller/TeamEventController.java
+++ src/main/java/cz/scarecrows/eventmanager/controller/TeamEventController.java
@@ -35,6 +35,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping(EVENTS)
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://scarecrows.cz/")
 public class TeamEventController {
 
     private final EntityMapper entityMapper;
