752a7a052ed8fffb719c03c39ca65b7fd7499b30
Petr Kadlec <petr.kadlec@finshape.com>
2023-09-06 22:18:48 +0200

    - default password settings

--- src/main/java/cz/scarecrows/eventmanager/data/request/MemberRegistrationRequest.java
+++ src/main/java/cz/scarecrows/eventmanager/data/request/MemberRegistrationRequest.java
@@ -1,16 +1,15 @@
 package cz.scarecrows.eventmanager.data.request;
 
-import java.io.Serializable;
-
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class MemberRegistrationRequest implements Serializable {
+public class MemberRegistrationRequest {
 
     private String appId;
     private String username;
+    private String password;
     private String authorities;
     private String registrationId;
 }
