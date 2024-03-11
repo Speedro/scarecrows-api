3e66d12f7a06b671bd488a48285722877410f666
Petr Kadlec <petr.kadlec@finshape.com>
2024-03-11 12:26:14 +0100

    Testing jenkins

--- src/test/java/cz/scarecrows/eventmanager/controller/TeamEventCreateControllerTest.java
+++ src/test/java/cz/scarecrows/eventmanager/controller/TeamEventCreateControllerTest.java
@@ -2,16 +2,10 @@ package cz.scarecrows.eventmanager.controller;
 
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.jdbc.Sql;
 
 import cz.scarecrows.eventmanager.AbstractIntegrationTest;
 import okhttp3.OkHttpClient;
